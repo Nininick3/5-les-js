66d6f288308e7d56983268f7b90b991b13651df8
Nick <kolyakoluch@gmail.com>
2023-05-13 18:46:28 +0300

    1com

---
+++ js.js
@@ -0,0 +1,11 @@
+function Press() {
+    document.getElementById('2').innerHTML='Press';
+}
+
+function second() {
+    document.getElementById('h2').innerHTML='LmaO';
+}
+
+function F() {
+    document.getElementById('1').innerHTML='F'
+}
